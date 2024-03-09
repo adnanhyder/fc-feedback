bb8d1661426e2a008cbb885b83b6af254b8ac150
adnanhyder <adnan.hyder@luqon.com>
2024-03-10 03:43:01 +0500

    configured and installed WordPress Docker Compose

---
+++ wp-app/wp-content/plugins/fc-feedback/fc-feedback.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: fc-feedback
+ * Plugin URI: #
+ * Description: WordPress plugin that allows website visitors to vote on various articles
+ * Version: 1.0.0
+ * Author: Adnan Hyder Pervez
+ * Author URI: #
+ * Developer: Adnan
+ * Developer URI: #
+ * Text Domain: fc
+ * Domain Path: /languages
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package FC-Feedback
+ */
